a2fb59a9faabe6bd7faf6ad8d06a20088cc1616d
Adam <adamgrahm@live.se>
2016-11-22 13:26:59 +0100

    Day 2 update #1

--- Project/App_Start/BundleConfig.cs
+++ Project/App_Start/BundleConfig.cs
@@ -27,7 +27,8 @@ namespace Project
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Custom.css"));
         }
     }
 }
