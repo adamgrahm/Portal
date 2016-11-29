d00b5814312a624af92626c34dfbe3b90e79f3e3
Adam <adamgrahm@live.se>
2016-11-29 16:12:46 +0100

    week 2 day 2 #1

--- Project/App_Start/BundleConfig.cs
+++ Project/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace Project
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/angular.js"));
+                        "~/Scripts/angular.js",
+                        "~/Scripts/site.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ Project/Scripts/site.js
@@ -0,0 +1,32 @@
+﻿//Visar och döljer en reply textarea för svar på första posten!
+(function () {
+    var app = angular.module("myModule", [])
+
+    app.controller("myController", function($scope)
+    {
+        $scope.testing = 'Write a reply...'
+        $scope.showReply = false;
+
+        $scope.show = function(Id)
+        {
+            $scope.showReply = true;
+            $("#textarea_" + Id).text('')
+        }
+        $scope.hide = function(Id)
+        {
+            $scope.showReply = false;
+            $("#textarea_" + Id).text('')
+        }
+        $scope.quote = function (Id)
+        {
+            var quoting = $("#" + Id).text();
+            $scope.showReply = true;
+            $scope.testing = quoting;
+            var thenum = Id.replace(/^\D+/g, '');
+            $("#textarea_" + thenum).text(quoting)
+            
+        }
+    })
+
+}());
+
